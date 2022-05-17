858f103306548699c7d4096dd88daf70e19d582b
Austin <austinow@gmail.com>
2022-05-17 13:26:12 -0700

    invert binary tree

--- problems.js
+++ problems.js
@@ -1,1 +1,21 @@
 console.log('hello')
+
+// * 973. K Closest Points to Origin(medium)
+//     * Sort the array
+//     * Splice the array
+//     * Create a variable that output the squared value
+ 
+// 226. Invert Binary Tree
+var invertTree = function(root) {
+    if( !root) return null;
+    
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+};
+
+invertTree([4,2,7,1,3,6,9])
