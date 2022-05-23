2f39f747e5bd31090886f7f628a7a15ab12378fe
Austin <austinow@gmail.com>
2022-05-23 15:22:31 -0700

    majorityElement

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Leetcode_progress
+<!-- // how to solve leetcode problems?
+
+1. to understand the problem
+2. diagram it and explain every single step
+3. pseudo code your coding steps
+4. code it out based on the psuedo code -->
+
--- problems.js
+++ problems.js
@@ -1,4 +1,18 @@
 console.log('hello')
+// take note?
+// 1. comment on your ConvolverNode, build this habit
+// - The amount of comments at the start should be commented to explain what they're doing.
+// as your get better, this will be needed less and less.
+// eventually you will only need to comment on important steps 
+
+// 2. write documentation.
+// every project you start should start with a readme
+// file. learn basic markdown, many markdown cheat sheets often. 
+// you'll build the habit of starting wtih readme files. 
+// see how other apps
+// Potential employers will look at github documentation will value this. 
+// A nice portfolio will have applications with explained comments and documentation.
+
 
 // * 973. K Closest Points to Origin(medium)
 //     * Sort the array
@@ -37,4 +51,25 @@ var groupAnagrams = function(strs) {
     }
     result = [...map.values()]
     return result;
+};
+
+// 169. Majority Element
+// hashmap
+// count the number if the key is the same.
+// if not in hashmap, set a new one with count 1
+//
+
+var majorityElement = function(nums) {
+    let map = new Map();
+    for(let i=0; i< nums.length; i++){
+        if(!map.has(nums[i])){
+            map.set(nums[i], 1)
+        } else {
+            map.set(nums[i], map.get(nums[i]) +1)
+        }
+    }
+    const sortedMap = new Map([...map.entries()].sort((a,b) =>  b[1] - a[1]) )
+    let key = sortedMap.keys().next().value
+    return key
+    
 };
