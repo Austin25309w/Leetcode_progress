df1b10ae304be8ea1c6f596bb683a40f9041e50b
Austin <austinow@gmail.com>
2023-12-19 18:29:27 -0800

    partition equal subset sum

--- problems.js
+++ problems.js
@@ -418,3 +418,28 @@ var wordBreak = function(s, wordDict) {
             }
         }
     }
+}
+
+
+// partition equal subset sum
+
+var canPartition = function(nums) {
+    const totalSum = nums.reduce((sum, num) => sum + num, 0);
+
+    // If the total sum is odd, it's not possible to partition into two equal subsets
+    if (totalSum % 2 !== 0) {
+        return false;
+    }
+
+    const targetSum = totalSum / 2;
+    const dp = new Array(targetSum + 1).fill(false);
+    dp[0] = true;
+
+    for (const num of nums) {
+        for (let i = targetSum; i >= num; i--) {
+            dp[i] = dp[i] || dp[i - num];
+        }
+    }
+
+    return dp[targetSum];
+};
