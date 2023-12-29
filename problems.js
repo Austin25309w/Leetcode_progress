fedd921e95ff38e861ef0d00498e2064b85b705d
Austin <austinow@gmail.com>
2023-12-28 22:51:40 -0800

    trap rain water

--- problems.js
+++ problems.js
@@ -650,3 +650,32 @@ var findAnagrams = function(s, p) {
     // Return the array 'res' containing the starting indices of anagrams of 'p' in 's'.
     return res;
 };
+
+// trapping rain water
+
+var trap = function(height) {
+    let result = 0;
+    let start = 0;
+    let end = height.length-1
+    let startMax = 0;
+    let endMax =0;
+
+    while(start < end){
+        if(height[start] < height[end]){
+            if(height[start] >= startMax){
+                startMax = height[start]
+            } else {
+                result += startMax - height[start];
+            }
+            start++;
+        } else {
+            if(height[end] >= endMax){
+                endMax = height[end]
+            } else {
+                result += endMax - height[end]
+            }
+            end--;
+        }
+    }
+    return result;
+};
