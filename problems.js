9264df166ac919153e059a5e9c5d6e3c55a46ade
Austin <austinow@gmail.com>
2024-01-05 22:38:42 -0800

    house robber

--- problems.js
+++ problems.js
@@ -788,3 +788,20 @@ var combinationSum = function(candidates, target) {
     backtrack([], target, 0);
     return combinations;
 };
+
+// House robber
+
+var rob = function(nums, idx = 0, memo = {}) {
+    if(idx in memo){
+        return memo[idx]
+    }
+    
+    if(idx >= nums.length){
+        return 0;
+    }
+
+    const sumIfSkipped = rob(nums, idx+1, memo);
+    const sumIfRobbed = nums[idx] + rob(nums, idx +2, memo)
+
+    return memo[idx] = Math.max(sumIfSkipped, sumIfRobbed)
+};
