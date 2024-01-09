3498c8ea1e6f6329bed0cc4805c7db70e8582193
Austin <austinow@gmail.com>
2024-01-08 16:24:13 -0800

    remove subarray

--- problems.js
+++ problems.js
@@ -843,15 +843,5 @@ var groupAnagrams = function(strs) {
 };
 
 
-7. Maximum subarray:
 
-var maxSubArray = function(nums) {
-    let sum = nums[0];
-    let maxSum = nums[0];
-    for(let i = 1; i<nums.length; i++){
-        sum = Math.max(nums[i], sum + nums[i])
-        maxSum = Math.max(sum, maxSum)
-    }
-    return maxSum
-};
 
