05accb6e68f4a75d64ce059f05d05c4ff475031a
Austin <austinow@gmail.com>
2024-01-08 16:22:48 -0800

    group anagrams

--- problems.js
+++ problems.js
@@ -827,4 +827,31 @@ const robHouse = (nums, i, endIdx, memo) => {
     return memo[i]
 }
 
+// Group Anagrams:
+
+var groupAnagrams = function(strs) {
+    let result = {};
+    for(let word of strs){
+        let cleansed = word.split("").sort().join("");
+        if(result[cleansed]){
+            result[cleansed].push(word)
+        }else {
+            result[cleansed] = [word]
+        }
+    }
+    return Object.values(result)
+};
+
+
+7. Maximum subarray:
+
+var maxSubArray = function(nums) {
+    let sum = nums[0];
+    let maxSum = nums[0];
+    for(let i = 1; i<nums.length; i++){
+        sum = Math.max(nums[i], sum + nums[i])
+        maxSum = Math.max(sum, maxSum)
+    }
+    return maxSum
+};
 
