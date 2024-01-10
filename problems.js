3dface5cf875982d8a4cc79ff87cb92c11af85d4
Austin <austinow@gmail.com>
2024-01-09 17:40:15 -0800

    maximum subarray

--- problems.js
+++ problems.js
@@ -845,3 +845,14 @@ var groupAnagrams = function(strs) {
 
 
 
+// Maximum subarray:
+
+var maxSubArray = function(nums) {
+    let sum = nums[0];
+    let maxSum = nums[0];
+    for(let i = 1; i<nums.length; i++){
+        sum = Math.max(nums[i], sum + nums[i])
+        maxSum = Math.max(sum, maxSum)
+    }
+    return maxSum
+};
