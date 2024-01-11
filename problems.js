7cf1c70928981612e15d5074e9e7c25b66c697dc
Austin <austinow@gmail.com>
2024-01-11 07:17:05 -0800

    longest increacing subsequence

--- problems.js
+++ problems.js
@@ -912,3 +912,23 @@ var spiralOrder = function(matrix) {
     }
     return result;
 };
+
+
+
+// Longest increase subsequence 
+
+var lengthOfLIS = function(nums) {
+    if(!nums) return 0;
+
+    let n = nums.length;
+    let dp = new Array(n).fill(1)
+
+    for(let i =1; i< n; i++){
+        for(let j = 0; j<i; j++){
+            if(nums[i] > nums[j]){
+                dp[i] = Math.max(dp[i], dp[j]+1)
+            }
+        }
+    }
+    return Math.max(...dp)
+};
