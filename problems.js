b569e55c7f9f2e361fd5b2b70fc9a5a865119e87
Austin <austinow@gmail.com>
2024-01-17 07:21:33 -0800

    jump game

--- problems.js
+++ problems.js
@@ -1044,3 +1044,21 @@ var kthSmallest = function(root, k) {
 
     return res; // Return the kth smallest element
 };
+
+
+// Jump game:
+
+var canJump = function(nums) {
+    let maxReach =  0;
+    for(let i =0; i<nums.length; i++){
+        if(i > maxReach){
+            return false;
+        }
+        maxReach = Math.max(maxReach, i+nums[i])
+
+        if(maxReach >= nums.length -1){
+            return true;
+        }
+    }
+    return true;
+};
