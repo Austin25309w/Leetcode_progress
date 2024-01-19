53741cc7d2fd5d4877306f9995e82e438b311385
Austin <austinow@gmail.com>
2024-01-18 16:46:44 -0800

    insert interval

--- problems.js
+++ problems.js
@@ -1062,3 +1062,22 @@ var canJump = function(nums) {
     }
     return true;
 };
+
+// Insert Interval
+var insert = function(intervals, newInterval) {
+    let [start, end] = newInterval;
+    let left =[];
+    let right = [];
+
+    for(let interval of intervals){
+        let [first, last] = interval;
+
+        if(last < start) left.push(interval);
+        else if(first > end) right.push(interval);
+        else {
+            start = Math.min(start, first);
+            end = Math.max(end, last);
+        }
+    }
+    return [...left, [start,end], ...right]
+};
