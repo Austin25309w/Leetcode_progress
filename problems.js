45ea9a04022aaa8c95e69d88ae4da694ab57a812
Austin <austinow@gmail.com>
2024-01-29 07:18:46 -0800

    climbing stairs with memo

--- problems.js
+++ problems.js
@@ -1379,3 +1379,16 @@ var kthSmallest = function(root, k) {
 
     return res
 };
+
+// climbing Stairs with memo
+
+var climbStairs = function(n, memo = {}) {
+    if(n in memo){
+        return memo[n]
+    }
+    if(n <= 1){
+        return 1
+    }
+    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2,memo)
+    return memo[n]
+};  
