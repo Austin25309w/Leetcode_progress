9959905b38ce65f84ddaf2a4845f7a3c9ee7ff38
Austin <austinow@gmail.com>
2024-02-08 16:52:01 -0800

    move zero to the end with swapping feature

--- problems.js
+++ problems.js
@@ -1748,3 +1748,16 @@ var countGoodSubstrings = function (s) {
   
     return unquieLength; // Return the count of good substrings
   };
+
+  // Move zeros to the end:
+
+var moveZeroes = function(nums) {
+    let left = 0
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[left], nums[i]] = [nums[i], nums[left]]
+            left++
+        }
+    }
+    return nums;
+};
