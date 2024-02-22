7dd04175fcf713c3fe8b385f6633e2b5bfc811c7
Austin <austinow@gmail.com>
2024-02-21 17:59:12 -0800

    longest increasing substring

--- problems.js
+++ problems.js
@@ -1935,3 +1935,19 @@ var rob = function(nums, idx = 0, memo = {}) {
 
 // 2/18/2024
 // create a next.js project here
+
+// longest increasing substring
+
+var lengthOfLIS = function(nums) {
+    if(!nums) return 0;
+    let n = nums.length;
+    let dp = new Array(n).fill(1);
+    for(let i=1; i<n; i++){
+        for(let j=0; j<i; j++){
+            if(nums[i] > nums[j]){
+                dp[i] = Math.max(dp[i], dp[j] +1)
+            }
+        }
+    }
+    return Math.max(...dp)
+};
