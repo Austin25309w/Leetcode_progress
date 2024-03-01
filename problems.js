deec09c3fad0b598827ecd94b3e50a01b3ca6597
Austin <austinow@gmail.com>
2024-02-29 17:56:27 -0800

    two sum o (n)

--- problems.js
+++ problems.js
@@ -1951,3 +1951,17 @@ var lengthOfLIS = function(nums) {
     }
     return Math.max(...dp)
 };
+
+// two sum with o(n)
+var twoSum = function(nums, target) {
+    //
+    let map = new Map()
+    for(let i =0; i< nums.length; i++){
+        let value = target - nums[i]
+        if(map.has(value)){
+            return [map.get(value), i]
+        }
+        map.set(nums[i], i)
+    }
+    return null
+};
